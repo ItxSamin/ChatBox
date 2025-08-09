ce70ad2bfb076e2845b6ca1b0a8feace780413d7
ItxSamin <samin8a@gmail.com>
2025-08-09 16:48:09 +0500

    Just a Basic Structure

---
+++ views/index.php
@@ -0,0 +1,67 @@
+<?php include 'includes/header.php';?>
+<div class="container">
+    <div class="sidebar">
+
+        <div class="header">
+            <div class="user_info">
+                <div class="user_pic">
+                    <img src="assets/img/profile_img.webp" alt="">
+                </div>
+                <div class="user_details">
+                    <h4>Samin</h4>
+                    <p>@samin_bhai</p>
+                </div>
+                <div class="icons">
+                    <i class="fa-regular fa-bell"></i>
+                </div>
+            </div>
+        </div>
+        <div class="messages_list">
+            <div class="heading">
+            <h4>Messages</h4>
+            <i><i class="fa-regular fa-message"></i></i>
+            </div>
+            <div class="search_area">
+
+            </div>
+            <div class="messages_pool">
+                <div class="messages_cont">
+                    <div class="message_card">
+                        <div class="sender_pic">
+                            <img src="assets/img/profile_img.webp" alt="">
+                        </div>
+                        <div class="sender_details">
+                            <h4>Hassam</h4>
+                            <p>Hello, how are you</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <div class="chat_area">
+        <div class="chat_display">
+            <div class="chat_header">
+                <div class="user_info">
+                    <div class="user_pic">
+                        <img src="assets/img/profile_img.webp" alt="">
+                    </div>
+                    <div class="user_details">
+                        <h4>Hassam</h4>
+                        <p>@janwar</p>
+                    </div>
+                </div>
+                <div class="icons">
+                    <i class="fa fa-video"></i>
+                    <i class="fa fa-phone"></i>
+                    <i class="fa fa-ellipsis-vertical"></i>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?php include 'includes/footer.php';?>
+    
